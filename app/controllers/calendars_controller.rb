46d0005f8ac52719595f9dd33565a15230e85b4c
1satoshik7 <satoshi.kaneko1207@gmail.com>
2020-10-15 17:30:08 +0900

    修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -16,7 +16,7 @@ class CalendarsController < ApplicationController
 
   # planに変更
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
